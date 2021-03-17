beb3c0614837ba87669eaab36d527778c099cba9
Noah Day <sheltielover432@gmail.com>
2021-03-16 22:35:30 -0400

    Splitting Nodes

--- Source/PGG_Unreal/Floor.cpp
+++ Source/PGG_Unreal/Floor.cpp
@@ -36,7 +36,13 @@ bool Floor::ShouldSplitNode(TSharedPtr<FloorNode> InNode, ESplitOrientation Orie
 	if (Orientation == ESplitOrientation::ESO_Horizontal)
 	{
 		int32 floorLength = CornerCoordinates.bottomRightY - CornerCoordinates.upperLeftY;
-		if (floorLength > roomMinY)
+		float percentChanceOfSplit = (float) floorLength/ (float) floorGridSizeX;
+		float chance = FMath::FRandRange(0.1f, 1.f);
+		if (chance > percentChanceOfSplit)
+		{
+			return false;
+		}
+		if (floorLength >= 2 * roomMinY)
 		{
 			return true;
 		}
@@ -44,7 +50,13 @@ bool Floor::ShouldSplitNode(TSharedPtr<FloorNode> InNode, ESplitOrientation Orie
 	else if (Orientation == ESplitOrientation::ESO_Vertical)
 	{
 		int32 floorWidth = CornerCoordinates.bottomRightX - CornerCoordinates.upperLeftX;
-		if (floorWidth > roomMinX)
+		float percentChanceOfSplit = floorWidth/floorGridSizeY;
+		float chance = FMath::FRandRange(0.1f, 1.f);
+		if (chance > percentChanceOfSplit)
+		{
+			return false;
+		}
+		if (floorWidth >=  2*roomMinX)
 		{
 			return true;
 		}
@@ -59,22 +71,38 @@ bool Floor::SplitAttempt(TSharedPtr<FloorNode> InNode)
 	{
 		if (ShouldSplitNode(InNode, ESplitOrientation::ESO_Horizontal))
 		{
-			
+			TSharedPtr<FloorNode> B(new FloorNode());
+			TSharedPtr<FloorNode> C(new FloorNode());
+
+			SplitHorizontal(InNode, B, C);
+			return true;
 		}
 		else if (ShouldSplitNode(InNode, ESplitOrientation::ESO_Vertical))
 		{
-			
+			TSharedPtr<FloorNode> B(new FloorNode());
+			TSharedPtr<FloorNode> C(new FloorNode());
+
+			SplitVertical(InNode, B, C);
+			return true;
 		}
 	}
-	else
+	else if(HorizontalOrVertical == 1)
 	{
 		if (ShouldSplitNode(InNode, ESplitOrientation::ESO_Vertical))
 		{
-			
+			TSharedPtr<FloorNode> B(new FloorNode());
+			TSharedPtr<FloorNode> C(new FloorNode());
+
+			SplitVertical(InNode, B, C);
+			return true;
 		}
 		else if (ShouldSplitNode(InNode, ESplitOrientation::ESO_Horizontal))
 		{
-			
+			TSharedPtr<FloorNode> B(new FloorNode());
+			TSharedPtr<FloorNode> C(new FloorNode());
+
+			SplitHorizontal(InNode, B, C);
+			return true;
 		}
 	}
 	return false;
@@ -82,10 +110,46 @@ bool Floor::SplitAttempt(TSharedPtr<FloorNode> InNode)
 
 void Floor::SplitHorizontal(TSharedPtr<FloorNode> InA, TSharedPtr<FloorNode> InB, TSharedPtr<FloorNode> InC)
 {
+	int32 splitPointY = FMath::RandRange(InA->GetCornerCoordinates().upperLeftY + roomMinY, InA->GetCornerCoordinates().bottomRightY - roomMinY);
+	FCornerCoordinates cornerCoordinatesB;
+	cornerCoordinatesB.upperLeftX = InA->GetCornerCoordinates().upperLeftX;
+	cornerCoordinatesB.upperLeftY = InA->GetCornerCoordinates().upperLeftY;
+	cornerCoordinatesB.bottomRightX = InA->GetCornerCoordinates().bottomRightX;
+	cornerCoordinatesB.bottomRightY = splitPointY;
+
+	InB->SetCornerCoordinates(cornerCoordinatesB);
+	PartionedFloor.Push(InB);
+	
+	FCornerCoordinates cornerCoordinatesC;
+	cornerCoordinatesC.upperLeftX = InA->GetCornerCoordinates().upperLeftX;
+	cornerCoordinatesC.upperLeftY = splitPointY;
+	cornerCoordinatesC.bottomRightY = InA->GetCornerCoordinates().bottomRightY;
+	cornerCoordinatesC.bottomRightX = InA->GetCornerCoordinates().bottomRightX;
+
+	InC->SetCornerCoordinates(cornerCoordinatesC);
+	PartionedFloor.Push(InC);
 }
 
 void Floor::SplitVertical(TSharedPtr<FloorNode> InA, TSharedPtr<FloorNode> InB, TSharedPtr<FloorNode> InC)
 {
+	int32 splitPointX = FMath::RandRange(InA->GetCornerCoordinates().upperLeftX + roomMinX, InA->GetCornerCoordinates().bottomRightX - roomMinX);
+	FCornerCoordinates cornerCoordinatesB;
+	cornerCoordinatesB.upperLeftX = InA->GetCornerCoordinates().upperLeftX;
+	cornerCoordinatesB.upperLeftY = InA->GetCornerCoordinates().upperLeftY;
+	cornerCoordinatesB.bottomRightX = splitPointX;
+	cornerCoordinatesB.bottomRightY = InA->GetCornerCoordinates().bottomRightY;
+
+	InB->SetCornerCoordinates(cornerCoordinatesB);
+	PartionedFloor.Push(InB);
+
+	FCornerCoordinates cornerCoordinatesC;
+	cornerCoordinatesC.upperLeftX = splitPointX;
+	cornerCoordinatesC.upperLeftY = InA->GetCornerCoordinates().upperLeftY;
+	cornerCoordinatesC.bottomRightY = InA->GetCornerCoordinates().bottomRightY;
+	cornerCoordinatesC.bottomRightX = InA->GetCornerCoordinates().bottomRightX;
+
+	InC->SetCornerCoordinates(cornerCoordinatesC);
+	PartionedFloor.Push(InC);
 }
 
 Floor::~Floor()
